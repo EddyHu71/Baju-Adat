0c9819a73f5a11d230a61ae2c5263c3fc9f2e058
Eddy <eddyhu71@gmail.com>
2019-08-24 15:00:30 +0700

    Second Commit

--- action/api/db.php
+++ action/api/db.php
@@ -3,7 +3,7 @@
     {
         public $con;
         public function __construct(){
-            $this->con = mysqli_connect("localhost", "u2401249_eddyhu71", "Eddyhu71", "u2401249_db_hor");
+            $this->con = mysqli_connect("localhost", "root", "", "db_bajuadat");
             if (!$this->con) {
                 echo "Error in Connecting ".mysqli_connect_error();
             }
---
+++ action/api/functions.php
@@ -0,0 +1,33 @@
+<?php
+	include "action.php";
+	$crud = new Crud();
+	
+	if (isset($_POST['user_login'])) {
+		$name_login = $_POST[''];
+		$user_login = $_POST[''];
+		$pass_login = $_POST[''];
+		$crud->fetchwhere("table_user", "'$user_login' = username_user AND '$pass_login' = password_user");
+		if ($crud) {
+			echo "Anda berhasil login";
+		} else {
+			echo "Anda gagal login";
+		}
+	}
+	
+	if (isset($_POST['user_register'])) {
+		$name_register = $_POST[''];
+		$user_register = $_POST[''];
+		$email_register = $_POST[''];
+		$pass_register = $_POST[''];
+		$phone_register = $_POST[''];
+		
+		$crud->fetchwhere("table_user", "'$user_register' = username_user AND '$email_register' = email_user");
+		if ($crud) {
+			echo "Register gagal";
+		} else {
+			echo "Register sukses";
+		}
+	}
+	
+	
+?>
