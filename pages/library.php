c667ac3567d96f07cf3326b2bb478bcedc031c70
Eddy <eddyhu71@gmail.com>
2019-11-20 16:43:26 +0700

    Change Template

--- pages/library.php
+++
@@ -1,5 +0,0 @@
-
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/boostrap-theme.css">
-	<link rel="stylesheet" href="css/boostrap-theme.min.css">
--- pages/libraryjs.php
+++
@@ -1,3 +0,0 @@
-	<script type="text/javascript" href="js/bootstrap.js"></script>
-	<script type="text/javascript" href="js/bootstrap.min.js"></script>
-	<script type="text/javascript" href="js/npm.js"></script>
